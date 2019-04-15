dc8b72d5393d712984f69c3964f492c664a6dfd2
中男顽童 <qgtxqj@gmail.com>
2019-04-15 16:18:49 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 # Configure V2Ray
-echo -e '{"log":{"loglevel":"none"},"inbounds":[{"port":9999,"protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]},"streamSettings":{"network":"kcp","kcpSettings":{"mtu":1350,"tti":100,"uplinkCapacity":10,"downlinkCapacity":50,"congestion":false,"readBufferSize":2,"writeBufferSize":2,"header":{"type":"wechat-video"}}}},{"port":9999,"protocol":"vmess","settings":{"clients":[{"id":"uuid","alterId":64}]}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > /v2ray/config.json
-sed -i "s/uuid/$UUID/g" /v2ray/config.json
-# Run V2Ray
-/v2ray/v2ray -config=/v2ray/config.json
+
+rm -f /etc/motd
+cat >>/etc/motd <<EOF
+Linux Version : $(cat /etc/redhat-release)
+Kernel Version : $(uname -r)
+Hostname : $(uname -n)
+Enjoy your Docker-Linux Node !
+EOF
+
+echo "root:${ROOT_PASSWORD}" | chpasswd
+
+echo "Start Success !"
+
+sed -i "s/uuid/$UUID/g" /etc/v2ray/config.json
+
+cat /etc/v2ray/config.json
+
+(/usr/sbin/sshd -D -e \
+	-o PermitRootLogin=yes \
+	-o Port=${SSH_PORT}) &
+
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
