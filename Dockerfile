9aaab42c6f97220544ba24991ffca262dfeb00bd
中男顽童 <qgtxqj@gmail.com>
2019-04-24 07:57:51 +0800

    /etc

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,17 @@ ENV UUID=dd4523c3-fa0a-4aa3-acfe-0a49c7f643ce VER=4.18.0 ROOT_PASSWORD=alpine SS
 
 RUN apk add --no-cache --virtual .build-deps busybox bash ca-certificates curl openssh-server openssh-sftp-server caddy \
  && ssh-keygen -A \
- && mkdir -m 777 /v2ray \
- && mkdir -m 777 /caddy \
- && mkdir -m 777 /caddy/www
+ && mkdir -m 777 /etc/v2ray \
+# && mkdir -m 777 /caddy \
+ && mkdir -m 777 /etc/caddy/www \
+ && mkdir -m 777 /usr/bin/v2ray
 
-ADD config.json /config.json
-ADD files/caddy.conf /caddy/caddy.conf
+ADD files/config.json /etc/v2ray/config.json
+ADD files/caddy.conf /etc/caddy/caddy.conf
 ADD files/authorized_keys /etc/ssh/authorized_keys
 RUN chmod 600 /etc/ssh/authorized_keys
 ADD files/sshd_config /etc/ssh/sshd_config
-ADD files/index.html /caddy/www/index.html
+ADD files/index.html /etc/caddy/www/index.html
 ADD run.sh /run.sh
 RUN chmod +x /run.sh
 ENTRYPOINT /run.sh
--- run.sh
+++ run.sh
@@ -8,25 +8,29 @@ EOF
 
 echo "root:${ROOT_PASSWORD}" | chpasswd
 
-sed -i "s/uuid/$UUID/g" /config.json
-sed -i "s/sspass/$SSPASS/g" /config.json
+sed -i "s/uuid/$UUID/g" /etc/v2ray/config.json
+sed -i "s/sspass/$SSPASS/g" /etc/v2ray/config.json
 
 #(/usr/sbin/sshd -D -e \
 #	-o PermitRootLogin=yes \
 #	-o Port=${SSH_PORT}) &
 /usr/sbin/sshd -D -e &
-/usr/sbin/caddy -conf /caddy/caddy.conf >/dev/null  2>&1  &
+/usr/sbin/caddy -conf /etc/caddy/caddy.conf >/dev/null  2>&1  &
 echo "Start sshd & caddy Success !"
-cd /v2ray
+cd /tmp
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray.zip 
-if [ ! -f "v2ray" ]; then
-  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
-  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
-  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
-  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
-fi
+unzip v2ray.zip "v2ray" -d /usr/bin/v2ray/
+unzip v2ray.zip "v2ctl" -d /usr/bin/v2ray/
+unzip v2ray.zip "geosite.dat" -d /usr/bin/v2ray/
+unzip v2ray.zip "geoip.dat" -d /usr/bin/v2ray/
 
-cp -f /config.json .
+#if [ ! -f "v2ray" ]; then
+#  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
+#  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
+#  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
+#  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
+#fi
+cd /usr/bin/v2ray
+#cp -f /config.json .
 chmod +x v2ray v2ctl
-/v2ray/v2ray -config=/v2ray/config.json
+./v2ray -config=/etc/v2ray/config.json
