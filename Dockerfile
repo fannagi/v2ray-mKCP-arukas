783f6240be683600d3d15fb0eba290e9810816b7
中男顽童 <qgtxqj@gmail.com>
2019-04-25 14:56:18 +0800

    fix crond

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:latest
 
 ENV UUID=uuid VER=4.18.0 ROOT_PASSWORD=alpine SSPASS=sspass UU1ID=uuid1 UU2ID=uuid2 UU3ID=uuid3 UU4ID=uuid4 UU5ID=uuid5 UU6ID=uuid6
 
-RUN apk add --no-cache --virtual .build-deps busybox bash ca-certificates curl openssh-server openssh-sftp-server caddy tzdata \
+RUN apk add --no-cache --virtual .build-deps busybox bash ca-certificates curl openssh-server openssh-sftp-server caddy tzdata nano \
  && ssh-keygen -A \
  && mkdir -m 755 /etc/v2ray \
  && mkdir -m 755 /etc/caddy/www \
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ Enjoy your Docker-Linux Node !
 EOF
 
 echo "root:${ROOT_PASSWORD}" | chpasswd
-sed -i '$i 0       12       *       *       *       cat /dev/null > /etc/v2ray/access.log' /etc/crontabs/root
+
 sed -i "s/uuid/$UUID/g" /etc/v2ray/config.json
 sed -i "s/uu1id/$UU1ID/g" /etc/v2ray/config.json
 sed -i "s/uu2id/$UU2ID/g" /etc/v2ray/config.json
@@ -16,13 +16,6 @@ sed -i "s/uu4id/$UU4ID/g" /etc/v2ray/config.json
 sed -i "s/uu5id/$UU5ID/g" /etc/v2ray/config.json
 sed -i "s/uu6id/$UU6ID/g" /etc/v2ray/config.json
 sed -i "s/sspass/$SSPASS/g" /etc/v2ray/config.json
-
-#(/usr/sbin/sshd -D -e \
-#	-f /etc/ssh/sshd_config \
-#	-o Port=${SSH_PORT}) &
-
-/usr/sbin/caddy -conf /etc/caddy/caddy.conf >/dev/null  2>&1  &
-echo "Start caddy Success !"
 cd /tmp
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray.zip "v2ray" -d /usr/bin/v2ray/
@@ -41,4 +34,12 @@ cd /usr/bin/v2ray
 chmod +x v2ray v2ctl
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json >/dev/null  2>&1  &
 echo "Start v2ray Success !"
-/usr/sbin/sshd -D -e
+
+/usr/sbin/caddy -conf /etc/caddy/caddy.conf >/dev/null  2>&1  &
+echo "Start caddy Success !"
+
+/usr/sbin/sshd
+echo "Start sshD Success !"
+
+sed -i '$i 0       12      *       *       *       cat /dev/null > /etc/v2ray/access.log' /etc/crontabs/root
+crond -l 2 -f
